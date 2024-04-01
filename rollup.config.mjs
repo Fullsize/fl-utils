6b75dbb6574023f116fffd6979a543d5467b67e7
无涯 <1619686166@qq.com>
2024-04-01 14:30:05 +0800

    chore: 完善package信息

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+// rollup.config.mjs
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+export default {
+  input: './src/index.ts', // 入口文件路径
+  output: [
+    {
+      name: 'fl-utils',
+      file: './lib/index.js', // 输出文件路径
+      format: 'es', // 输出模块格式为 CommonJS
+      sourcemap: false, // 生成 source map
+    },
+    {
+      name: 'fl-utils',
+      file: './lib/index.umd.js', // 输出文件路径
+      format: 'umd', // 输出模块格式为 CommonJS
+      sourcemap: false, // 生成 source map
+    }
+  ],
+  plugins: [
+    typescript(), // 使用 Rollup 插件处理 TypeScript
+    terser()
+  ],
+  external: ['react'], // 将 react 设置为外部依赖，不会打包到最终文件中
+};
